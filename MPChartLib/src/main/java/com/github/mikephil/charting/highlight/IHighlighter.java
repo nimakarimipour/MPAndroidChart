e0e4a4a2032178930d04274e4c62bc04fd23a488
nimakarimipour <karimipour.nima@gmail.com>
2022-04-09 18:22:18 -0700

    Final Result

--- MPChartLib/src/main/java/com/github/mikephil/charting/highlight/IHighlighter.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/highlight/IHighlighter.java
@@ -1,10 +1,11 @@
 package com.github.mikephil.charting.highlight;
 
+import androidx.annotation.Nullable;
+
 /**
  * Created by philipp on 10/06/16.
  */
-public interface IHighlighter
-{
+public interface IHighlighter {
 
     /**
      * Returns a Highlight object corresponding to the given x- and y- touch positions in pixels.
@@ -13,5 +14,6 @@ public interface IHighlighter
      * @param y
      * @return
      */
+    @Nullable
     Highlight getHighlight(float x, float y);
 }
